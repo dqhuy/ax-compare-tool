fcca69d0e720ea537ab11543398c13525c0bb372
Huy Dinh <quanghuyqn@gmail.com>
2022-11-22 14:10:18 +0700

    Add project files.

---
+++ OCRFullText.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace ax_tool
+{
+    partial class OCRFullText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtImageFolder = new System.Windows.Forms.TextBox();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.btnOpenImageFolder = new System.Windows.Forms.Button();
+            this.btnOpenOutputFolder = new System.Windows.Forms.Button();
+            this.progressBarOCR = new System.Windows.Forms.ProgressBar();
+            this.btnRunOCR = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.backgroundWorkerOCR = new System.ComponentModel.BackgroundWorker();
+            this.btnSelectPreprocessedFolder = new System.Windows.Forms.Button();
+            this.txtPreprocessedImageFolder = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOCRServer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Compare OCR Quality";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Orginal Image folder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output folder";
+            // 
+            // txtImageFolder
+            // 
+            this.txtImageFolder.Location = new System.Drawing.Point(31, 137);
+            this.txtImageFolder.Name = "txtImageFolder";
+            this.txtImageFolder.Size = new System.Drawing.Size(499, 22);
+            this.txtImageFolder.TabIndex = 3;
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(31, 240);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(499, 22);
+            this.txtOutputFolder.TabIndex = 4;
+            // 
+            // btnOpenImageFolder
+            // 
+            this.btnOpenImageFolder.Location = new System.Drawing.Point(551, 137);
+            this.btnOpenImageFolder.Name = "btnOpenImageFolder";
+            this.btnOpenImageFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenImageFolder.TabIndex = 5;
+            this.btnOpenImageFolder.Text = "Select";
+            this.btnOpenImageFolder.UseVisualStyleBackColor = true;
+            this.btnOpenImageFolder.Click += new System.EventHandler(this.btnOpenImageFolder_Click);
+            // 
+            // btnOpenOutputFolder
+            // 
+            this.btnOpenOutputFolder.Location = new System.Drawing.Point(551, 240);
+            this.btnOpenOutputFolder.Name = "btnOpenOutputFolder";
+            this.btnOpenOutputFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenOutputFolder.TabIndex = 6;
+            this.btnOpenOutputFolder.Text = "Select";
+            this.btnOpenOutputFolder.UseVisualStyleBackColor = true;
+            this.btnOpenOutputFolder.Click += new System.EventHandler(this.btnOpenOutputFolder_Click);
+            // 
+            // progressBarOCR
+            // 
+            this.progressBarOCR.Location = new System.Drawing.Point(31, 293);
+            this.progressBarOCR.Name = "progressBarOCR";
+            this.progressBarOCR.Size = new System.Drawing.Size(499, 23);
+            this.progressBarOCR.TabIndex = 7;
+            // 
+            // btnRunOCR
+            // 
+            this.btnRunOCR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRunOCR.Location = new System.Drawing.Point(551, 293);
+            this.btnRunOCR.Name = "btnRunOCR";
+            this.btnRunOCR.Size = new System.Drawing.Size(75, 31);
+            this.btnRunOCR.TabIndex = 9;
+            this.btnRunOCR.Text = "Run";
+            this.btnRunOCR.UseVisualStyleBackColor = true;
+            this.btnRunOCR.Click += new System.EventHandler(this.btnRunOCR_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(28, 273);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(65, 17);
+            this.lbStatus.TabIndex = 10;
+            this.lbStatus.Text = "Progress";
+            // 
+            // backgroundWorkerOCR
+            // 
+            this.backgroundWorkerOCR.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerOCR_DoWork);
+            this.backgroundWorkerOCR.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerOCR_ProgressChanged);
+            this.backgroundWorkerOCR.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerOCR_RunWorkerCompleted);
+            // 
+            // btnSelectPreprocessedFolder
+            // 
+            this.btnSelectPreprocessedFolder.Location = new System.Drawing.Point(551, 189);
+            this.btnSelectPreprocessedFolder.Name = "btnSelectPreprocessedFolder";
+            this.btnSelectPreprocessedFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPreprocessedFolder.TabIndex = 13;
+            this.btnSelectPreprocessedFolder.Text = "Select";
+            this.btnSelectPreprocessedFolder.UseVisualStyleBackColor = true;
+            this.btnSelectPreprocessedFolder.Click += new System.EventHandler(this.btnSelectPreprocessedFolder_Click);
+            // 
+            // txtPreprocessedImageFolder
+            // 
+            this.txtPreprocessedImageFolder.Location = new System.Drawing.Point(31, 189);
+            this.txtPreprocessedImageFolder.Name = "txtPreprocessedImageFolder";
+            this.txtPreprocessedImageFolder.Size = new System.Drawing.Size(499, 22);
+            this.txtPreprocessedImageFolder.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(183, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Pre-processed image folder";
+            // 
+            // txtOCRServer
+            // 
+            this.txtOCRServer.Location = new System.Drawing.Point(31, 81);
+            this.txtOCRServer.Name = "txtOCRServer";
+            this.txtOCRServer.Size = new System.Drawing.Size(499, 22);
+            this.txtOCRServer.TabIndex = 15;
+            this.txtOCRServer.Text = "192.168.6.90";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(223, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "AX Server (Set empty to run local)";
+            // 
+            // OCRFullText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 586);
+            this.Controls.Add(this.txtOCRServer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSelectPreprocessedFolder);
+            this.Controls.Add(this.txtPreprocessedImageFolder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnRunOCR);
+            this.Controls.Add(this.progressBarOCR);
+            this.Controls.Add(this.btnOpenOutputFolder);
+            this.Controls.Add(this.btnOpenImageFolder);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.txtImageFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "OCRFullText";
+            this.Text = "OCR Full text";
+            this.Load += new System.EventHandler(this.OCRFullText_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtImageFolder;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.Button btnOpenImageFolder;
+        private System.Windows.Forms.Button btnOpenOutputFolder;
+        private System.Windows.Forms.ProgressBar progressBarOCR;
+        private System.Windows.Forms.Button btnRunOCR;
+        private System.Windows.Forms.Label lbStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerOCR;
+        private System.Windows.Forms.Button btnSelectPreprocessedFolder;
+        private System.Windows.Forms.TextBox txtPreprocessedImageFolder;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOCRServer;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ OCRFullText.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using AX.AXSDK;
+using OneAPI;
+
+namespace ax_tool
+{
+    public partial class OCRFullText : Form
+    {
+        public OCRFullText()
+        {
+            InitializeComponent();
+        }
+
+        private string[] processedData = new string[3] { string.Empty, string.Empty, string.Empty };
+        private DateTime startTime;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorkerOCR_DoWork(object sender, DoWorkEventArgs e)
+        {
+            backgroundWorkerOCR.WorkerReportsProgress = true;
+            runOCR(e);
+        }
+
+        /// <summary>
+        /// Do OCR and compare result quality of orginal image vs pre-processed image
+        /// Then write result to cvs file to output folder
+        /// </summary>
+        private void runOCR(DoWorkEventArgs e)
+        {
+
+            try
+            {
+                string imageFolder = txtImageFolder.Text;
+                string preprocessedImageFolder = txtPreprocessedImageFolder.Text;
+                string resultFolder = txtOutputFolder.Text;
+
+                //loop all file => run ocr then compare result
+                string resultFilePath = Path.Combine(resultFolder, "ocr-result.csv");
+                if (File.Exists(resultFilePath))
+                {
+                    File.Delete(resultFilePath);
+                }
+
+                StreamWriter streamWriter = new StreamWriter(resultFilePath);
+                streamWriter.AutoFlush = true;
+
+                streamWriter.WriteLine(string.Join(",", new string[]{
+                    "File",
+                    "Total_word",
+                    "Average_Confidence",
+                    "Total_word_2",
+                    "Average_Confidence_2",
+                    "Delta_word",
+                    "Delta_Confidence"
+                }));
+
+                int totalFile = Directory.EnumerateFiles(imageFolder).Count();
+                int fileIndex = 0;
+                if (totalFile == 0)
+                {
+                    MessageBox.Show("There is no image in folder: " + imageFolder);
+                    return;
+                }
+                foreach (var f in Directory.EnumerateFiles(imageFolder).
+                    Where(i => i.EndsWith(".jpg") ||
+                    i.EndsWith(".png") ||
+                    i.EndsWith("pdf")))
+                {
+                    FileInfo orginalfileInfo = new FileInfo(f);
+                    FileInfo preprocessedFileInfo = new FileInfo(Path.Combine(preprocessedImageFolder, orginalfileInfo.Name));
+
+
+                    fileIndex++;
+                    int progressValue = 100 * fileIndex / totalFile;
+                    processedData[0] = fileIndex.ToString();
+                    processedData[1] = totalFile.ToString();
+                    processedData[2] = orginalfileInfo.Name;
+                    backgroundWorkerOCR.ReportProgress(progressValue);
+
+
+
+                    //check pre-processed file exist by name
+                    //if not exist preprocessed file then do nothing
+                    if (!File.Exists(preprocessedFileInfo.FullName))
+                    {
+                        continue;
+                    }
+
+
+
+                    List<OCRResult> ocrResults = APIs.API.RecognizeVBHC(orginalfileInfo.FullName);
+                    List<OCRResult> ocrResults2 = APIs.API.RecognizeVBHC(preprocessedFileInfo.FullName);
+
+                    //output record format
+                    // filename, totalword, averageConfidence
+                    long totalWord, totalWord2, deltaWord = 0;
+                    double averageConfi, averageConfi2, deltaConfi = 0.0;
+
+                    averageOCRResult(ocrResults, out totalWord, out averageConfi);
+                    averageOCRResult(ocrResults2, out totalWord2, out averageConfi2);
+
+                    deltaWord = totalWord2 - totalWord;
+                    deltaConfi = averageConfi2 - averageConfi;
+
+                    string filename = new FileInfo(f).Name;
+                    string resultText = string.Join(",", new string[] {
+                        filename,
+                        totalWord.ToString(),
+                        averageConfi.ToString(),
+                        totalWord2.ToString(),
+                        averageConfi2.ToString(),
+                        deltaWord.ToString(),
+                        deltaConfi.ToString()
+                    });
+
+                    streamWriter.WriteLine(resultText);
+                }
+                //close file
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                e.Cancel = true;
+            }
+        }
+
+        private void averageOCRResult(List<OCRResult> ocrResults, out long totalWord, out double averageConfidence)
+        {
+            totalWord = 0;
+            averageConfidence = 0;
+            double totalConfi = 0;
+
+            foreach (var r in ocrResults)
+            {
+                if (r.WordInfos.Count() > 0)
+                {
+                    totalWord += r.WordInfos.Count();
+                    totalConfi += r.WordInfos.Sum(x => x.Confidence);
+                }
+
+            }
+            if (totalWord != 0)
+            {
+                averageConfidence = totalConfi / totalWord;
+            }
+        }
+
+        private void backgroundWorkerOCR_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int totalprocessed = int.Parse(processedData[0]);
+            DateTime endTime = DateTime.Now;
+            Double elapsed = ((TimeSpan)(endTime - startTime)).TotalSeconds;
+            double speed = elapsed/totalprocessed;
+
+            progressBarOCR.Value = e.ProgressPercentage;
+            lbStatus.Text = processedData[0] + " / " + processedData[1] + "- File: " + processedData[2];
+            if (totalprocessed > 1)
+            {
+                lbStatus.Text += " - Everage time(s): "+ String.Format("{0:0.0}", speed);
+            }
+            else
+            {
+                lbStatus.Text += " - Everage time(s): NaN";
+            }
+        }
+
+        private void backgroundWorkerOCR_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lbStatus.Text = lbStatus.Text + " DONE ";
+            btnRunOCR.Enabled = true; ;
+        }
+
+        private void btnOpenImageFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtImageFolder.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void btnOpenOutputFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputFolder.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void btnRunOCR_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(txtImageFolder.Text))
+            {
+                MessageBox.Show("Image folder is not exist!");
+                return;
+            }
+            if (!System.IO.Directory.Exists(txtOutputFolder.Text))
+            {
+                MessageBox.Show("Output folder is not exist!");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(txtOCRServer.Text))
+            {
+                APIs.SetServerAddress("192.168.6.90");
+            }
+            //Everything is ready
+            lbStatus.Text = string.Empty;
+            progressBarOCR.Value = 0;
+            btnRunOCR.Enabled = false;
+            startTime = DateTime.Now;
+
+            backgroundWorkerOCR.RunWorkerAsync();
+        }
+
+        private void btnSelectPreprocessedFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtPreprocessedImageFolder.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void OCRFullText_Load(object sender, EventArgs e)
+        {
+            setDefaultValeForDebug();
+        }
+
+        private void setDefaultValeForDebug()
+        {
+            txtOCRServer.Text = "192.168.6.90";
+            txtImageFolder.Text = "D:\\Google-drive-huy-work\\imagedata\\skew\\train\\images";
+            txtPreprocessedImageFolder.Text = "D:\\Google-drive-huy-work\\imagedata\\skew\\train\\images\\out";
+            txtOutputFolder.Text = "D:\\Google-drive-huy-work\\imagedata\\skew\\train\\ocr-compare";
+
+            txtOCRServer.Text = string.Empty;// "192.168.6.90";
+            txtImageFolder.Text = "\\\\huydq-pc\\imagedata\\skew\\train\\images";
+            txtPreprocessedImageFolder.Text = "\\\\huydq-pc\\imagedata\\skew\\train\\images\\out";
+            txtOutputFolder.Text = "\\\\huydq-pc\\imagedata\\skew\\train\\ocr-compare";
+
+
+        }
+    }
+}
